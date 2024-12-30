49b398260b38e33d6d585c9feb3475a42a5230be
Anirudh Sardiwal <anirudhsardiwal@gmail.com>
2024-12-30 14:18:45 +0400

    initial attempt at deployment

--- heart_attack_pred_app.py
+++ heart_attack_pred_app.py
@@ -9,18 +9,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("Adult.html")
+    return render_template("heart.html")
 
 
 def ValuePredictor(to_predict_list):
-    to_predict = np.array(to_predict_list).reshape(1, 12)
-    loaded_model = pickle.load(open("adult_flask.pkl", "rb"))
+    to_predict = np.array(to_predict_list).reshape(1, 15)
+    loaded_model = pickle.load(open("heart_attack_model.pkl", "rb"))
     result = loaded_model.predict(to_predict)
     return result[0]
 
 
-@app.route("/result", methods=["POST"])
-def result():
+@app.route("/upload_csv", methods=["POST"])
+def upload_csv():
     if request.method == "POST":
         if "csv_file" in request.files:
             file = request.files["csv_file"]
@@ -32,19 +32,24 @@ def result():
                 result_file_path = os.path.join("uploads", "predictions.csv")
                 predictions.to_csv(result_file_path, index=False)
                 return send_file(result_file_path, as_attachment=True)
-        to_predict_list = request.form.to_dict()
-        to_predict_list = list(to_predict_list.values())
-        to_predict_list = list(map(int, to_predict_list))
-        result = ValuePredictor(to_predict_list)
-        if int(result) == 1:
-            prediction = "Income more than 50k"
-        else:
-            prediction = "Income less than 50k"
-        return render_template("result.html", prediction=prediction)
+        return "No file uploaded"
+
+
+@app.route("/result", methods=["POST"])
+def result():
+    to_predict_list = request.form.to_dict()
+    to_predict_list = list(to_predict_list.values())
+    to_predict_list = list(map(int, to_predict_list))
+    result = ValuePredictor(to_predict_list)
+    if int(result) == 1:
+        prediction = "High chances of Heart Attack"
+    else:
+        prediction = "Heart Attack not likely"
+    return render_template("result.html", prediction=prediction)
 
 
 def bulk_predict(data):
-    loaded_model = pickle.load(open("adult_flask.pkl", "rb"))
+    loaded_model = pickle.load(open("heart_attack_model.pkl", "rb"))
     predictions = loaded_model.predict(data)
     data["prediction"] = predictions
     return data
